904570219dadb3262cb7812da3eb4f19ac5fde86
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-03-09 22:05:42 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,21 +2,21 @@
 #=========================================================================================================================
 
 
-# 1-添加ShadowSocksR Plus+插件
+# 1-添加 ShadowSocksR Plus+ 插件
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 2-添加PowerOff关机插件
-git clone https://github.com/BTC-LaoMa/luci-app-poweroff.git package/luci-app-poweroff
+# 2-添加 poweroff 插件
+git clone https://github.com/MIT-X/luci-app-poweroff.git package/luci-app-poweroff
 
-# 3-添加 adguardhome 插件
-git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adguardhome
+# 3-添加 opentomcat 主题
+git clone https://github.com/MIT-X/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
-# 4-添加OpenClash插件
+# 4-添加 adguardhome 插件
+# git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adguardhome
+
+# 5-添加OpenClash插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
-# 5-添加OpenTomcat主题
-git clone https://github.com/BTC-LaoMa/luci-theme-opentomcat.git package/luci-theme-opentomcat
-
 # 6-添加PassWall插件
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
