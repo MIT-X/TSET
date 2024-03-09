5ef4a1aced95a6f9b4b68079406034691aec67ab
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-03-09 19:38:56 +0800

    Update diy-1.sh

--- diy-1.sh
+++ diy-1.sh
@@ -8,15 +8,15 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 添加PowerOff关机插件
 git clone https://github.com/BTC-LaoMa/luci-app-poweroff.git package/luci-app-poweroff
 
-# 添加OpenTomcat主题
-git clone https://github.com/BTC-LaoMa/luci-theme-opentomcat.git package/luci-theme-opentomcat
+# 6-添加 adguardhome 插件
+git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adguardhome
 
 # 添加OpenClash插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
+# 添加OpenTomcat主题
+git clone https://github.com/BTC-LaoMa/luci-theme-opentomcat.git package/luci-theme-opentomcat
+
 # 添加PassWall插件
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-
-# 6-添加 adguardhome 插件
-git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adguardhome
