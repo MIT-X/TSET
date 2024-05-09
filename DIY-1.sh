78123d104e2111164aa4495a450529626db3612e
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-05-09 08:37:12 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -14,12 +14,16 @@ git clone https://github.com/MIT-X/luci-theme-opentomcat.git package/luci-theme-
 # 4-添加 adguardhome 插件
 # git clone https://github.com/MIT-X/luci-app-adguardhome.git package/luci-app-adguardhome
 
-# 5-添加OpenClash插件
+# 5-添加 OpenClash 插件
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 
-# 6-添加PassWall插件
+# 6-添加 PassWall 插件
 # echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 # echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
+# 7-添加 MosDNS 插件
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
